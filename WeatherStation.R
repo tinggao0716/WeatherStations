9089796a2fff390944b656da0117983323cf16f7
Ting Gao <tinggao0716@gmail.com>
2014-03-18 15:55:30 +0000

    Refine the inner data of the S4 class

--- WeatherStation.R
+++ WeatherStation.R
@@ -1,47 +1,67 @@
 library(RgoogleMaps)
 
 setClass("WeatherStation", 
-         slots = c(Lat = "numeric", Lon = "numeric", Distance = "integer"))
+         slots = c(stationdata = "data.frame"),
+         prototype = list(
+                          stationdata=data.frame(
+                                                 City=character(),
+                                                 State=character(), 
+                                                 Country=character(),
+                                                 Id=character(),
+                                                 Lat=numeric(),
+                                                 Lon=numeric(),
+                                                 Distance=numeric())))
 
 .WeatherStation.valid <- function(object) {
-  len <- length(object@Lat)
-  if(len != length(object@Lon) || len != length(object@Distance)) {
-    return("length mismatch")
+  if (!is.character(object@stationdata$City)){
+    return("City type mismatch")  
+  }
+  if(!is.character(object@stationdata$State)){
+    return("State type mismatch")  
+  }
+  if(!is.character(object@stationdata$Country)){
+    return("Country type mismatch")  
+  }
+  if( !is.character(object@stationdata$Id)){
+    return("Id type mismatch")  
+  }
+  if( !is.numeric(object@stationdata$Lat)){
+    return("Lat type mismatch")  
+  }
+  if(!is.numeric(object@stationdata$Lon)){
+    return("Lon type mismatch")  
+  }
+  if(!is.numeric(object@stationdata$Distance)){
+    return("Distance type mismatch")  
   }
   return(TRUE)
 }
 
 setValidity("WeatherStation", .WeatherStation.valid)
 
-"WeatherStation" <- function(Lat=double(), Lon=double(), Distance=integer()){
-  new("WeatherStation",Lat=as.numeric(Lat),Lon = as.numeric(Lon), Distance=as.integer(Distance))
+"WeatherStation" <- function(
+                             stationdata=data.frame(
+                                                    City=character(),
+                                                    State=character(), 
+                                                    Country=character(),
+                                                    Id=character(),
+                                                    Lat=numeric(),
+                                                    Lon=numeric(),
+                                                    Distance=numeric())){ 
+  new("WeatherStation",stationdata=stationdata)
 }
 
 setMethod("subset", "WeatherStation", function(x, dis = 40) {
-          r <- data.frame(x@Lat, x@Lon, x@Distance)
-          colnames(r) <- c("Lat", "Lon", "Distance")
-          r <- r[r$Distance < dis, ]
-          WeatherStation(r$Lat, r$Lon, r$Distance)
-         })
-
-plot.WeatherStation <- function(x, ...){
-  gx <- x@Lon
-  gy <- x@Lat
-  mymap <- MapBackground(lat=gy, lon=gx) 
-  PlotOnStaticMap(mymap, lat=gy, lon=gx,
-                  destfile = "weatherstations.png", cex=1.5,pch=20, col="red")
-}
-
-setGeneric("plot")
-setMethod("plot", c(x = "WeatherStation", y = "missing"), plot.WeatherStation)
+          r <- x@stationdata[x@stationdata$Distance < dis, ]
+          WeatherStation(r)
+                             })
 
 .getDf.WeatherStation <- function(x, ...) {
-  r <- data.frame(x@Lat, x@Lon, x@Distance)
-  colnames(r) <- c("Lat", "Lon", "Distance")
-  r
+  x@stationdata
 }
 
 setGeneric("getDf",function(x){standardGeneric("getDf")})
 setMethod("getDf", "WeatherStation", function(x) {
           .getDf.WeatherStation(x)
-                  })
+                             })
+
--- tests/test_WeatherStation.R
+++ tests/test_WeatherStation.R
@@ -1,17 +1,33 @@
 library(testthat)
 
+createTestDf <- function(){
+  data.frame(
+             City= c("A", "BC"),
+             State=c("CA", "IL"),
+             Country=c("Santa Clara", "Evanston"),
+             Id=c("af4", "safsAS"),
+             Lat=c(12.3, 432.241),
+             Lon=c(34.5, 908.1),
+             Distance=c(2,5),
+             stringsAsFactors=FALSE)
+}
+
 test_that("Create WeatherStation correctly", {
-  ws <- WeatherStation(c(1.2, 2.3), c(2.3, 4.6), c(2, 5))
-  expect_that(nrow(getDf(ws)), equals(2))
-})
+          df <- createTestDf()
+          ws<-WeatherStation(df)
+          expect_that(nrow(getDf(ws)), equals(2))
+             })
 
 test_that("Create WeatherStation wrongly", {
-  expect_that(WeatherStation(c(1.2, 2.3), c(4.6), c(2, 5)), throws_error())
-})
+          df <- createTestDf()
+          df$Lon <- c("sadfa", "jf093")
+          expect_that(WeatherStation(df), throws_error())
+             })
 
 test_that("WeatherStation subset", {
-  ws <- WeatherStation(c(1.2, 2.3), c(2.3, 4.6), c(2, 5))
-  ss <- subset(ws, 3)
-  expect_that(nrow(getDf(ss)), equals(1))
-})
+          df <- createTestDf()
+          ws<-WeatherStation(df)
+          ss <- subset(ws, 3)
+          expect_that(nrow(getDf(ss)), equals(1))
+             })
 
